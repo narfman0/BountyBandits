d4f566e8681bf16e2799922af4bd3f551f41f441
narfman0 <narfman0@f34f5d6a-d8fa-4a85-8d7d-3577574e89da>
2011-11-17 04:41:37 +0000

    killing bb trunk to dump 3d crap
    git-svn-id: file:///media/data/other/svn/repos/BountyBandits/trunk@5 f34f5d6a-d8fa-4a85-8d7d-3577574e89da

--- BountyBandits/Inventory/Class.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace BountyBandits.Inventory
-{
-    public enum Class { Normal, Magic, Rare, Unique };
-}
--- BountyBandits/Inventory/Type.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace BountyBandits.Inventory
-{
-    public enum Type { Head, Chest, MainHand, OffHand, Legs };
-}
